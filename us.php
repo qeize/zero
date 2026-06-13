2eff469103ddf538c730f73587d0a04cf3376a07
黄 <107405609+qeize@users.noreply.github.com>
2026-06-13 19:58:00 +0700

    Add files via upload

--- us.php
+++ us.php
@@ -533,7 +533,7 @@ body{background: black;}
 #loginbox td { border-radius:5px; font-size:11px; }
 </style>
 <title>~ Death Networks Shell-v'.__ALFA_VERSION__.'-'.__ALFA_CODE_NAME__.' ~</title><center>
-<center><img style="border-radius:100px;" width="500" height="250" alt="Death Networks 2012" draggable="false" src="https://raw.githubusercontent.com/qeize/tools/refs/heads/main/logo.png" /></center>
+<center><img style="border-radius:100px;" width="500" height="250" alt="Death Networks 2012" draggable="false" src="https://raw.githubusercontent.com/qeize/death/refs/heads/main/logo.png" /></center>
 <div id=loginbox><p><font face="verdana,arial" size=-1>
 <center><table cellpadding=\'2\' cellspacing=\'0\' border=\'0\' id=\'ap_table\'>
 <tr><td bgcolor="green"><table cellpadding=\'0\' cellspacing=\'0\' border=\'0\' width=\'100%\'><tr><td bgcolor="green" align=center style="padding:2;padding-bottom:4"><b><font color="white" size=-1 color="white" face="verdana,arial"><b>~ Death Networks Shell-v'.__ALFA_VERSION__.'-'.__ALFA_CODE_NAME__.' ~</b></font></th></tr>
@@ -675,7 +675,7 @@ img {
 </head>
 <body>
 
-<img src="https://raw.githubusercontent.com/qeize/tools/refs/heads/main/logo.gif" draggable="false">
+<img src="https://raw.githubusercontent.com/qeize/death/refs/heads/main/logo.gif" draggable="false">
 
 <div id="typedtext"></div>
 
@@ -1273,7 +1273,7 @@ echo '<input type="hidden" name="charset">
 <tr>
 <td><span class="header_vars">User:</span></td>
 <td><span class="header_values" id="header_userid">'. $uid . ' [ ' . $user . ' ] </span><span class="header_vars"> Group: </span><span class="header_values" id="header_groupid">' . $gid . ' [ ' . $group . ' ]</span> </td>
-<td width="12%" rowspan="8"><img style="border-radius:100px;" width="250" height="250" alt="Death Networks 2012" draggable="false" src="https://raw.githubusercontent.com/qeize/tools/refs/heads/main/logo.png" /></td>
+<td width="12%" rowspan="8"><img style="border-radius:100px;" width="250" height="250" alt="Death Networks 2012" draggable="false" src="https://raw.githubusercontent.com/qeize/death/refs/heads/main/logo.png" /></td>
 </tr>
 <tr>
 <td><span class="header_vars">PHP:</span></td>
